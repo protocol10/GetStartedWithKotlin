de78f45697441e25ee43462d1c815821cf2ffd0c
akshay.mukadam <akshay.mukadam@go-jek.com>
2017-10-08 17:18:43 +0530

    Added examples that demonstrates the use of functions in kotlin, with named parameters and other use cases

---
+++ src/chapter3/FunctionsDemo.kt
@@ -0,0 +1,58 @@
+package chapter3
+
+import java.lang.StringBuilder
+
+/**
+ * Created by akshaymukadam on 06/10/17.
+ */
+fun main(ars: Array<String>) {
+
+    val set = setOf<Int>(1, 2, 3, 4)
+    println(set.javaClass)
+
+    val list = listOf<String>("Abd", "DEF", "GHI")
+    println(list.javaClass)
+
+
+    val string = listOf<String>("james", "Sadio", "Albert")
+
+    println(string.size)
+
+
+    val numbers = setOf(1, 2, 4, 5, 3, 1)
+    println("Min number is ${numbers.min()}  and max number is${numbers.max()}")
+
+    println("JoinToString" + joinToString(numbers, prefix = "|", seperator = ",", postFix = "|"))
+    val lstNu = listOf<Int>(1, 2, 2, 3, 4, 5, 6, 6)
+
+    println("JoinToString Optional Params ${joinToString(lstNu)}")
+    println("Extension Functions" + lstNu.joinString(",", "[", "]"))
+}
+
+fun <T> joinToString(collection: Collection<T>, seperator: String = ",", prefix: String = "{", postFix: String = "}"): String {
+
+
+    val builder = StringBuilder(prefix);
+
+    for ((i, e) in collection.withIndex()) {
+        if (i > 0) {
+            builder.append(seperator)
+        }
+        builder.append(e)
+    }
+    builder.append(postFix)
+    return builder.toString()
+}
+
+fun <Int> Collection<Int>.joinString(seperator: String = ",", prefix: String = "{", postFix: String = "}"): String {
+
+    val builder = StringBuilder(prefix)
+    for ((index, element) in withIndex()) {
+        if (index > 0) {
+            builder.append(seperator)
+        }
+        builder.append(element)
+    }
+    builder.append(postFix)
+    return builder.toString()
+}
